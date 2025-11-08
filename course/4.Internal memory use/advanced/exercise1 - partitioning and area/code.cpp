7960621a531982af3e166bd9d90bd93f795d8b8e
AJMM11 <ajmartinmanzano@gmail.com>
2025-11-08 14:31:44 +0100

    Update code.cpp

--- course/4.Internal memory use/advanced/exercise1 - partitioning and area/code.cpp
+++ course/4.Internal memory use/advanced/exercise1 - partitioning and area/code.cpp
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-#define ROWS 8
-#define COLS 64
-void datapath1(int A[ROWS][COLS], int B[ROWS][COLS]) {
-    for (int r = 0; r < ROWS; r++) {
-        for (int c = 0; c < COLS; c++) {
-            #pragma HLS PIPELINE II=1
-            #pragma HLS UNROLL factor=16
-            B[r][c] = A[r][c] * 2;
-        }
-=======
 #define N 128
 
 void partition_exercise_opt(int A[N], int B[N]) {
@@ -16,6 +5,6 @@ void partition_exercise_opt(int A[N], int B[N]) {
         #pragma HLS PIPELINE II=1
         int sum = A[i] + A[i + 1] + A[i + 2];
         B[i] = sum;
->>>>>>> Stashed changes
+
     }
 }
